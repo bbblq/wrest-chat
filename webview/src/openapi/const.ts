626b7f31a40928685e60c581e08242b5a86ed405
bbblq <30345004+bbblq@users.noreply.github.com>
2024-09-12 17:34:33 +0800

    Update const.ts

--- webview/src/openapi/const.ts
+++ webview/src/openapi/const.ts
@@ -50,7 +50,7 @@ export const AiModels: Record<string, AiOptionData> = {
         endpoint: 'https://generativelanguage.googleapis.com',
         keystyle: '',
         models: {
-            'gemini-1.5-flash': 'gemini-1.5-flash',
+            'gemini-1.5-flash-001': 'gemini-1.5-flash-001',
         },
     },
     openai: {
